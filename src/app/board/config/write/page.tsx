93eb32f294dd9d57e75acca68b86b3bb11467f51
SilenceTale <chomn55@gmail.com>
2025-02-11 13:20:05 +0900

    .
    .

--- src/app/board/config/write/page.tsx
+++ src/app/board/config/write/page.tsx
@@ -3,11 +3,12 @@ import ConfigContainer from '../containers/ConfigContainer'
 import { MainTitle } from '@/app/global/components/StyledTitle'
 
 const WritePage = () => {
-  return
-  ;<>
-    <MainTitle>게시판 등록</MainTitle>
-    <ConfigContainer />
-  </>
+  return (
+    <>
+      <MainTitle>게시판 등록</MainTitle>
+      <ConfigContainer />
+    </>
+  )
 }
 
 export default React.memo(WritePage)
